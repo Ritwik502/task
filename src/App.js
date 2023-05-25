87d04978c8f71ff33e8277eb064f46cb788be5c7
Kumar Ritwik <kumarritwik502@gmail.com>
2023-05-25 23:50:21 +0530

    make second input boolean
    checks if vaue of drop is "true", defaut is true.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 function App() {
-  const [data,setData]=useState([{vname:"",lname:""}])
+  const [data,setData]=useState([{vname:"",boolVal:true}])
   const handleClick=()=>{
-      setData([...data,{vname:"",lname:""}])
+      setData([...data,{vname:"",boolVal:true}])
   }
   const handleChange=(e,i)=>{
       const {name,value}=e.target
       const onchangeVal = [...data]
-      onchangeVal[i][name]=value
+      if(name==="boolVal"){
+
+        onchangeVal[i][name]=(value==="true");
+      }
+      else onchangeVal[i][name]=value
       setData(onchangeVal)
   }
   const handleDelete=(i)=>{
@@ -21,7 +25,10 @@ function App() {
             data.map((val,i)=>
             <div>
                   <input name="vname" value={val.vname} onChange={(e)=>handleChange(e,i)} />
-                  <input name="lname" value={val.lname} onChange={(e)=>handleChange(e,i)} />
+                  <select  name ="boolVal" value={val.boolVal} onChange={(e)=>handleChange(e,i)}>
+                  <option value="true">True</option>
+                  <option value="false">False</option>
+                  </select>
                   <button onClick={()=>handleDelete(i)}>Delete</button>
               </div>
               )
