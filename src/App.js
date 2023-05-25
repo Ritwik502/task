27bdf16da93714b8e505c30abc411086db542ad7
Kumar Ritwik <kumarritwik502@gmail.com>
2023-05-25 23:14:17 +0530

    initial commit
    two text input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data,setData]=useState([{vname:"",lname:""}])
+  const handleClick=()=>{
+      setData([...data,{vname:"",lname:""}])
+  }
+  const handleChange=(e,i)=>{
+      const {name,value}=e.target
+      const onchangeVal = [...data]
+      onchangeVal[i][name]=value
+      setData(onchangeVal)
+  }
+  const handleDelete=(i)=>{
+      const deleteVal = [...data]
+      deleteVal.splice(i,1)
+      setData(deleteVal)
+  }
+  return(
+      <div className="App">
+          {
+            data.map((val,i)=>
+            <div>
+                  <input name="vname" value={val.vname} onChange={(e)=>handleChange(e,i)} />
+                  <input name="lname" value={val.lname} onChange={(e)=>handleChange(e,i)} />
+                  <button onClick={()=>handleDelete(i)}>Delete</button>
+              </div>
+              )
+            }
+            <button onClick={handleClick}>Add</button>
+          <p>{JSON.stringify(data)}</p>
+      </div>
+    )
 }
+
 
 export default App;
