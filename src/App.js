7006bfaa86234e6eab5f02db50d2d3c3335e4a65
Kumar Ritwik <kumarritwik502@gmail.com>
2023-05-26 20:37:18 +0530

    operator options
    added simple operator options
    with operation usestate

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import React, { useState } from "react";
 function App() {
-  const [data,setData]=useState([{vname:"",boolVal:true}])
-  const handleClick=()=>{
-      setData([...data,{vname:"",boolVal:true}])
+  const [data, setData] = useState([{ vname: "", boolVal: true }])
+  const [operation, setOperation] = useState()
+  const handleClick = () => {
+    setData([...data, { vname: "", boolVal: true }])
   }
-  const handleChange=(e,i)=>{
-      const {name,value}=e.target
-      const onchangeVal = [...data]
-      if(name==="boolVal"){
+  const handleChangeVar = (e, i) => {
+    const { name, value } = e.target
+    const onchangeVal = [...data]
+    if (name === "boolVal") {
 
-        onchangeVal[i][name]=(value==="true");
+      onchangeVal[i][name] = (value === "true");
+    }
+    else onchangeVal[i][name] = value
+    setData(onchangeVal)
+  }
+  const handleChangeOp = (e) => {
+    setOperation(e.target.value)
+  }
+  const handleDelete = (i) => {
+    const deleteVal = [...data]
+    deleteVal.splice(i, 1)
+    setData(deleteVal)
+  }
+  return (
+    <div className="App">
+      {
+        data.map((val, i) =>
+          <div>
+            <input name="vname" value={val.vname} onChange={(e) => handleChangeVar(e, i)} />
+            <select name="boolVal" value={val.boolVal} onChange={(e) => handleChangeVar(e, i)}>
+              <option value="true">True</option>
+              <option value="false">False</option>
+            </select>
+            <button onClick={() => handleDelete(i)}>Delete</button>
+          </div>
+        )
       }
-      else onchangeVal[i][name]=value
-      setData(onchangeVal)
-  }
-  const handleDelete=(i)=>{
-      const deleteVal = [...data]
-      deleteVal.splice(i,1)
-      setData(deleteVal)
-  }
-  return(
-      <div className="App">
-          {
-            data.map((val,i)=>
-            <div>
-                  <input name="vname" value={val.vname} onChange={(e)=>handleChange(e,i)} />
-                  <select  name ="boolVal" value={val.boolVal} onChange={(e)=>handleChange(e,i)}>
-                  <option value="true">True</option>
-                  <option value="false">False</option>
-                  </select>
-                  <button onClick={()=>handleDelete(i)}>Delete</button>
-              </div>
-              )
-            }
-            <button onClick={handleClick}>Add</button>
-          <p>{JSON.stringify(data)}</p>
+      <button onClick={handleClick}>Add</button>
+
+      <div>
+        <div>
+          <select name="operation " value={operation} onChange={(e) => handleChangeOp(e)}>
+              <option value="none">select...</option>
+              <option value="constant">constant</option>
+              <option value="argument">argument</option>
+              <option value="and">and</option>
+              <option value="or">or</option>
+            </select>
+        <button >X</button>
+        </div>
       </div>
-    )
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  )
 }
 
 
